06895a2de7c3da1de42f3fc8e823be72e5bd6555
watasho358 <t8mmmxc11y@yahoo.co.jp>
2026-02-22 11:26:41 +0900

    自動スクロール対応

--- components/messages.tsx
+++ components/messages.tsx
@@ -1,15 +1,22 @@
-// メッセージの一覧を表示するコンポーネント
+import { useScrollToBottom } from "@/hooks/use-scroll-to-bottom";
 import { Message } from "./message";
+
 export function Messages({ messages }: any) {
+  // 自動スクロール用の設定
+  const [messagesContainerRef, messagesEndRef] =
+    useScrollToBottom<HTMLDivElement>();
+
   return (
     <div
-      className="flex flex-col min-w-0 gap-6 flex-1 overflow-y-scroll pt-→
-4"
+      ref={messagesContainerRef}
+      className="flex flex-col min-w-0 gap-6 flex-1 overflow-y-scroll pt-4"
     >
-      {messages.map((message) => (
+      {messages.map((message: any) => (
         <Message key={message.id} message={message} />
       ))}
-      <div className="shrink-0 min-w-[24px] min-h-[24px]" />
+
+      {/* スクロール位置の終端を示す要素 */}
+      <div ref={messagesEndRef} className="shrink-0 min-w-6 min-h-6" />
     </div>
   );
 }
